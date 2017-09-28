12f6073cf3325b4a531dfec216340e8cc9717d42
louis.sussan@gmail.com <louis.sussan@gmail.com>
2017-09-27 21:02:47 -0700

    about page update

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,20 @@
-//******************************************************************
-//CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
-//******************************************************************
+// //******************************************************************
+// //CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
+// //******************************************************************
 
-//Dependencies
-var Sequelize = require("sequelize");
+// //Dependencies
+// var Sequelize = require("sequelize");
 
-//Created mySQL connection using Sequelize
-//Edit this object to use your mySQL user/database information
-var sequelize = new Sequelize("ClientDirectory", "root", "password", {
-    host: "iwqrvsv8e5fz4uni.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    dialect: "mysql",
-    pool: {
-        max: 5,
-        min: 0,
-        idle: 1000
-    }
-});
+// //Created mySQL connection using Sequelize
+// //Edit this object to use your mySQL user/database information
+// var sequelize = new Sequelize("ClientDirectory", "root", "fdkjjr49oj2gw7kd", {
+//     host: "iwqrvsv8e5fz4uni.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//     dialect: "mysql",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         idle: 1000
+//     }
+// });
 
-module.exports = sequelize;
+// module.exports = sequelize;
